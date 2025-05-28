9f52390d299c56deafc70a9537bbecf59b219b67
rameez-hussain <rmz.hussain55@gmail.com>
2025-05-28 12:50:22 +0100

    add more modules

---
+++ src/Modules/Events.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Avant\ZohoCRM\Modules;
+
+readonly class Events extends Module
+{
+}
---
+++ src/Records/Message.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Avant\ZohoCRM\Records;
+
+use Avant\ZohoCRM\Records\Traits\HasPhoneAttribute;
+
+class Message extends Record
+{
+    use HasPhoneAttribute;
+}
